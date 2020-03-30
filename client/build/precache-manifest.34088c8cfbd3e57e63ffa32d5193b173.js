40e316184dabbf01851e5f558306f236efb2fef6
MHane952 <haneymason22@gmail.com>
2020-03-30 13:17:45 -0500

    initial commit

---
+++ client/build/precache-manifest.34088c8cfbd3e57e63ffa32d5193b173.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1c31bcff8c32ea971e9d",
+    "url": "/static/js/main.949352fc.chunk.js"
+  },
+  {
+    "revision": "343543bd3c72a1e6609b",
+    "url": "/static/js/2.8223f89a.chunk.js"
+  },
+  {
+    "revision": "1c31bcff8c32ea971e9d",
+    "url": "/static/css/main.175cde4f.chunk.css"
+  },
+  {
+    "revision": "bb43480b8dfb421a4faffdc875f7ace8",
+    "url": "/index.html"
+  }
+];
